5461309a0fb8f568c2f5c014e82e06b789e96bd5
soheil-h <habibian@vt.edu>
2023-08-31 14:20:15 -0400

    Update: viz for SARI

--- SARI_panda/viz.py
+++ SARI_panda/viz.py
@@ -39,7 +39,7 @@ class VizServer(VizClass):
         return self.send(*args)
 
 
-class VizClient(VizServer):
+class VizClient(VizClass):
     def listen2comms(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
